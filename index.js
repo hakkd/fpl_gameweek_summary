d94af90eed6d23d1ff8fd30f3dbdf57b9384c4a8
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2023-10-23 18:12:15 -0700

    added default view

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req,res) => {
-    res.render("index.ejs");
+    try {
+        const response = await axios.get("https://fantasy.premierleague.com/api/bootstrap-static/");
+        const result = response.data.events;
+        console.log(result);
+        res.render("index.ejs", { data: result, selected: null});
+    } catch (error) {
+        res.status(404).send(error);
+    }
 })
+
+app.post("/", async (req, res) => {
+    try {
+        console.log(req.body.gw);
+        const response = await axios.get("https://fantasy.premierleague.com/api/bootstrap-static/");
+        const result = response.data.events;
+        const gw = req.body.gw;
+        const selected_gw = result.filter(
+            data => {
+                return data.id == gw;
+            }
+        );
+        res.render("index.ejs", { data: result, selected: selected_gw});
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
